04f4c76f1cc53016a83add5e8272b5ce677cc82b
Kevin Wasonga <kevinwasonga415@outlook.com>
2023-04-30 22:09:21 +0300

    updating contact with formdata

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import Root, {loader as rootLoader, action as rootAction} from "./routes/root"
 import "./index.css"
 import ErrorPage from "./error-page"
 import Contact, {loader as contactLoader} from "./routes/contact"
-import EditContact from "./routes/edit"
+import EditContact, {action as editAction} from "./routes/edit"
 
 const router = createBrowserRouter([
  {
@@ -27,6 +27,7 @@ const router = createBrowserRouter([
     path: "contacts/:contactId/edit",
     element: <EditContact />,
     loader: contactLoader,
+    action: editAction,
    },
   ],
  },
